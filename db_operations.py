11ce1fbb5faefd1fe6ec2fa5b0c239792c36d181
pharsh2525 <p.harsh2525@gmail.com>
2023-11-22 05:59:38 -0600

    score update

--- db_operations.py
+++ db_operations.py
@@ -1,13 +1,24 @@
-import sqlite3
+"""
+ Name: Harshkumar Patel & Brenan Harman
+ Date: 16 November 2023
+ Project: Weather Processing App
+"""
+
 import logging
 from dbcm import DBCM
 
 class DBOperations:
+    """
+    A class for database creation.
+    """
     def __init__(self):
         self.db_name = "WeatherProcessor.db"
         self.initialize_db()
 
     def initialize_db(self):
+        """
+        create table is its not already created.
+        """
         create_table_query = '''
             CREATE TABLE IF NOT EXISTS weather_data (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -24,6 +35,9 @@ class DBOperations:
             logging.info("Database initialized.")
 
     def save_data(self, weather_data):
+        """
+        Inserts data into the table
+        """
         insert_query = '''
             INSERT OR IGNORE INTO weather_data (sample_date, location, min_temp, max_temp, avg_temp)
             VALUES (?, ?, ?, ?, ?)
@@ -36,6 +50,9 @@ class DBOperations:
             logging.info("Data saved to database.")
 
     def fetch_data(self, start_date, end_date):
+        """
+        Gets data from table
+        """
         fetch_query = '''
             SELECT * FROM weather_data
             WHERE sample_date BETWEEN ? AND ?
@@ -46,6 +63,9 @@ class DBOperations:
         return tuple(results)  # Convert the list of rows to a tuple
 
     def purge_data(self):
+        """
+        Deletes table from table
+        """
         purge_query = 'DELETE FROM weather_data'
         with DBCM(self.db_name) as cursor:
             cursor.execute(purge_query)
